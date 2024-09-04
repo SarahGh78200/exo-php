fdc1a9d78fcd97e1291f74c9ba72fe967a106efe
unknown <you@example.com>
2024-09-04 11:22:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'partials/head.php';
+
+require 'partials/footer.php';
+?>
---
+++ partials/footer.php
@@ -0,0 +1,5 @@
+<footer class="mt-5">
+        <p>Le poles <?php echo date("Y")?><p>
+</footer>
+</body>
+</html>
